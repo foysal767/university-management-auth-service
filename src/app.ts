15864ed9291d1ea70f77bd413127ebc001da3c98
foysal767 <uddinmohammadfoysal981@gmail.com>
2023-06-13 21:59:22 +0600

    setup logger done

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,29 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express';
 
-import cors from 'cors'
+import cors from 'cors';
 
-import usersRouter from './app/modules/users/users.route'
-import usersService from './app/modules/users/users.service'
-const app: Application = express()
-app.use(cors())
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
+
+const app: Application = express();
+app.use(cors());
 // console.log(app.get('env'))
 //perser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //application route
-app.use('/api/v1/users/', usersRouter)
+app.use('/api/v1/users/', UserRoutes);
 
-app.get('/', async (req: Request, res: Response) => {
-  await usersService.createUser({
-    id: '999',
-    role: 'student',
-    password: '1234',
-  })
-  res.send('Working successfully!')
-})
+//testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   //   throw new ApiError(400, 'ore baba, new error')
+//   //   Promise.reject(new Error('Unhandled Promise Rejection'))
+//   console.log(x)
 
-export default app
+// next('ore baba, new error')
+// })
+
+//Global error handler
+app.use(globalErrorHandler);
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,45 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorlogger, logger } from './shared/logger';
 
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    console.log(`🎯 Database is connected successfully`)
-    app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
-    })
+    await mongoose.connect(config.database_url as string);
+    logger.info(`🎯 Database is connected successfully`);
+    server = app.listen(config.port, () => {
+      logger.info(`Application listening on port ${config.port}`);
+    });
   } catch (error) {
-    console.log('Failed to connect database', error)
+    errorlogger.error('Failed to connect database', error);
   }
+
+  process.on('unhandledRejection', error => {
+    console.log(
+      'Unheandled Rejection is detected, we are closing our server .....'
+    );
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
-bootstrap()
+bootstrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
