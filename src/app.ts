03ad751f4d2f36375fdbeedb26d2158d3cefbd49
foysal767 <uddinmohammadfoysal981@gmail.com>
2023-06-15 22:50:05 +0600

    [Done] create academic interface and model & optimize res, tryCatch, route pattern

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { UserRoutes } from './app/modules/user/user.route';
+import routes from './app/routes';
 
 const app: Application = express();
 app.use(cors());
@@ -12,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//application route
-app.use('/api/v1/users/', UserRoutes);
+app.use('/api/v1/', routes);
 
 //testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
