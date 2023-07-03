226f952057754890db67b2f71800c59b43eca82f
foysal767 <uddinmohammadfoysal981@gmail.com>
2023-07-03 22:03:25 +0600

    Faculty, Admin added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -11,4 +11,16 @@ router.post(
   UserController.createStudent
 );
 
+router.post(
+  '/create-faculty',
+  validateRequest(UserValidation.createFacultyZodSchema),
+  UserController.createFaculy
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(UserValidation.createAdminZodSchema),
+  UserController.createAdmin
+);
+
 export const UserRoutes = router;
